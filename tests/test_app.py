e2e546be82b590b92efee9499901c290a04495e0
Başar <bsr4Ks@users.noreply.github.com>
2025-08-28 11:53:38 +0300

    modify Jenkinsfile

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,10 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from app import app
 from unittest.mock import patch
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 def login(client, username='admin', password='secret'):
     return client.post('/login', data={
         'username': username,
